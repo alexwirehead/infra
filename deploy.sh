88267113991a7a5bac8386cc01379225b5707853
alexwirehead <alex@wirehead.pro>
2019-06-14 14:01:24 +0300

    fix: fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+source ~/.rvm/scripts/rvm
 echo "Fetch source from git"
 git clone https://github.com/Artemmkin/reddit.git ~/reddit
-cd reddit
+cd ~/reddit
 echo "Install dependencies"
 bundle install
 echo "Start Puma Server"
