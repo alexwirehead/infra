456e746065576cbdeb89ea216f7abfb9539065a5
alexwirehead <alex@wirehead.pro>
2019-06-21 11:02:31 +0300

    feat: добавил конфигурацию firewall

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,7 @@ resource "google_compute_instance" "app" {
   machine_type = "g1-small"
   zone = "europe-west1-b"
   # определим загрузочный диск
+  tags = ["reddit-app"]
   boot_disk {
     initialize_params {
       image = "reddit-base-1561099838"
@@ -23,5 +24,20 @@ resource "google_compute_instance" "app" {
   metadata = {
     sshKeys = "appuser:${file("~/.ssh/appuser.pub")}"
   }
+  
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+  network = "default"
+  # указываем порты которые открываем
+  allow {
+    protocol = "tcp"
+    ports = ["9292"]
+  }
+  # для каких адресов открываем доступ
+  source_ranges = ["0.0.0.0/0"]
+  # для каких тегов применимо правило
+  target_tags = ["reddit-app"]
   
 }
