c1cf4031c24bf7907e9e35a02a73dfa41976700c
alexwirehead <alex@wirehead.pro>
2019-06-21 09:59:21 +0300

    feat: Создания инстанса из base образа
    Добавил конфигурацию для создания инстанса из base image

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,21 @@ provider "google" {
   region = "europe-west1"
   
 }
+
+resource "google_compute_instance" "app" {
+  name = "reddit-app"
+  machine_type = "g1-small"
+  zone = "europe-west1-b"
+  # определим загрузочный диск
+  boot_disk {
+    initialize_params {
+      image = "reddit-base-1561099838"
+    }
+  }
+  # определим сетевой интерфейс
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+  
+}
