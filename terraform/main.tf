a16492c245dd13cd5fa68a258b9769d23e7b620f
alexwirehead <alex@wirehead.pro>
2019-06-23 15:33:58 +0300

    feat: Добавлена конфигурация правила firewall

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,5 +61,17 @@ resource "google_compute_firewall" "firewall_puma" {
   source_ranges = ["0.0.0.0/0"]
   # для каких тегов применимо правило
   target_tags = ["reddit-app"]
+}
 
+
+resource "google_compute_firewall" "firewall_ssh" {
+  name = "default-allow-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
 }
