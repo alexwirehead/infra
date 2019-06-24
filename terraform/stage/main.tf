2040e42a3140489fe7da3a58558217b1e8fb90dc
alexwirehead <alex@wirehead.pro>
2019-06-24 10:08:33 +0300

    feat: добавил две среды
    Конфигурация вдух сред stage и prod

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -5,7 +5,7 @@ provider "google" {
 
 
 module "app" {
-  source = "./modules/app"
+  source = "../modules/app"
   app_disk_image = "${var.app_disk_image}"
   public_key_path = "${var.public_key_path}"
   private_key_path = "${var.private_key_path}"
@@ -13,12 +13,12 @@ module "app" {
 
 
 module "db" {
-  source = "./modules/db"
+  source = "../modules/db"
   db_disk_image = "${var.db_disk_image}"
   public_key_path = "${var.public_key_path}"
 }
 
 module "vps" {
-  source = "./modules/vpc"
+  source = "../modules/vpc"
   source_ranges = ["0.0.0.0/0"]
 }
