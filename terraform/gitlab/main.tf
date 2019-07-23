ee8ada83bbca569aef4d3ed0e4364e40070d5d55
alexwirehead <alex@wirehead.pro>
2019-07-23 11:16:23 +0300

    fix: обявление ip

--- terraform/gitlab/main.tf
+++ terraform/gitlab/main.tf
@@ -24,7 +24,7 @@ resource "google_compute_instance" "gitlab" {
   network_interface {
     network = "default"
     access_config {
-      nat_ip = "${google_compute_address.gitlab-ip.address}"
+      nat_ip = "${google_compute_address.gitlab_ip.address}"
     }
   }
   metadata = {
